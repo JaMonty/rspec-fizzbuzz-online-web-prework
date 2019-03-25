57a01a105ee82fb866a6ff485dfd35913e2d5dbb
Jason  Montes <jasone.montes@gmail.com>
2019-03-25 06:52:42 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,8 +3,16 @@
 
 
   def fizzbuzz(int)
-  if int % 3 == 0 # if the number int is divisible by 3
-    "Fizz" # Go fizz
+  if int % 15 == 0 
+    return "Fizzbuzz"
+  elsif int % 3 == 0  
+    return "Fizz"
+  elsif int % 5 == 0 
+    return "Buzz"
+    
+  else 
+    return
+    
   end
 end
 
